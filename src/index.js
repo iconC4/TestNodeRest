5c65896e095c3d9b69131b2cf650961e329ff1f1
iconC4 <cfour2545@gmail.com>
2023-03-16 17:15:01 +0700

    Update now

--- src/index.js
+++ src/index.js
@@ -25,5 +25,71 @@ console.log(user.calculateAge());
 // Object Cloning
 const obj1 = {
     person: 'Wachirayan',
-    weight: 65
+    weight: 60
 }
+
+const obj2 = { ...obj1 };
+obj2.weight = 65;
+console.log(obj1);
+console.log(obj2);
+
+// ITERATOR
+for (let char of "cfour")
+    console.log(char);
+
+// CLASS
+class Myclass {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    calculate() {
+        console.log('calculate your Age');
+    }
+}
+
+const myClass = new Myclass('CFOUR', 21);
+myClass.calculate();
+console.log(myClass.age);
+
+//CLASS EXTENDS
+class Media{
+    constructor(info){
+        this.publishDate = info.publishDate;
+        this.name = info.name;
+    }
+}
+class Song extends Media{
+    constructor(songData){
+        super(songData);
+        this.dev = songData.dev;
+    }
+}
+const mySong = new Song({
+    dev : "CFOUR",
+    name : "Wachirayan Engchuan",
+    publishDate: 2002
+});
+
+console.log(mySong);
+
+// Try ...Carch SYNTAX
+try{
+    //code..
+    throw"that is thrown every time";
+}catch(err){
+    // error handling
+    console.log("Have an Error");
+    //alert(err); // err on webpage
+    //alert(err.name); // err on webpage
+    //alert(err.message); // err on webpage
+    //alert(err.stack); // err on webpage
+}finally{
+    console.log("Code that always will run");
+}
+
+// ORIGINALLY USING CALLBACK
+function callback(){
+    console.log('Timeout completed');
+}
+setTimeout(callback, 3000); // wait 3 seconds
